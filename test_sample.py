ce3d6f1b6d764c7c766147c46ea96083c856f1d2
Tyler Martin <tylernm@gmail.com>
2014-04-10 17:37:39 -0700

    first commit

---
+++ test_sample.py
@@ -0,0 +1,13 @@
+import funcs
+
+
+def test_answer():
+	assert funcs.increment(3) == 5
+
+def test_answer2():
+	assert funcs.increment(4) == 5
+
+def test_answer3():
+	assert funcs.increment(2) == 3
+
+print "Heres a print statement..."
